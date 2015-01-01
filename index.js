4cc591d2593670683b15f6e1e1a054361c8a5ff0
yapcheahshen <yapcheahshen@gmail.com>
2015-01-01 23:49:33 +0800

    move into src, add --bare for browserify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var runtime=require("ksana2015-webruntime");
 runtime.boot("_%name%_","main","main",function(){
-	var Main=React.createElement(require("./main.jsx"));
+	var Main=React.createElement(require("./src/main.jsx"));
 	ksana.mainComponent=React.render(Main,document.getElementById("main"));
 });
