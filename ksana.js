c361db42560c5a77fbabfb883fa3f99d98bd7259
yapcheahshen <yapcheahshen@gmail.com>
2015-01-01 22:52:43 +0800

    use ksana.js instead of ksanafile.js

---
+++ ksana.js
@@ -0,0 +1,11 @@
+jsonp_handler({
+"version": "1",
+"build": 1,
+"title":"_%name%_",
+"minruntime": 4,
+"baseurl":"_%rawgit%_",
+"description":""
+"files":
+  ["index.html","build.js","build.css","jquery.js","react-with-addons.js","ksana.js",
+  "_%name%_.kdb"]
+})
