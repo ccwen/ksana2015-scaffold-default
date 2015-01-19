309874658b539647f47abd7facd5a743f2af7731
yapcheahshen <yapcheahshen@gmail.com>
2015-01-19 14:22:05 +0800

    better sample

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,21 @@ var maincomponent = React.createClass({
   getInitialState:function() {
   	return {result:[]};
   },
-  componentDidMount:function() {
-  	kse.search("sample","資生",{range:{start:0}},function(err,data){
+  search:function() {
+  	var tofind=this.refs.tofind.getDOMNode().value;
+  	kse.search("sample",tofind,{range:{start:0}},function(err,data){
   		this.setState({result:data.excerpt});
   	},this);
   },
+  renderItem:function(item) {
+  	return <div dangerouslySetInnerHTML={{__html:item.text}}></div>
+  },
   render: function() {
-    return <div>Hello {this.state.result}</div>;
+    return <div><input ref="tofind" value="君子"></input>
+    <button onClick={this.search} >Search</button>
+    	{this.state.result.map(this.renderItem)}
+
+    </div>;
   }
 });
 module.exports=maincomponent;
